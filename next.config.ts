eaf3dbf378ffe2d6e17f9bdd156abcdd36d34a36
emamur <emrahmamur@gmail.com>
2025-11-20 22:39:58 +0300

    PWA support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit({
+  dest: "public",       // PWA dosyaları public klasörüne gidecek
+  register: true,       // Otomatik kayıt et
+  skipWaiting: true,    // Güncelleme gelince hemen uygula
+  disable: process.env.NODE_ENV === "development", // Geliştirme modunda PWA'yı kapat (kafamızı karıştırmasın)
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
